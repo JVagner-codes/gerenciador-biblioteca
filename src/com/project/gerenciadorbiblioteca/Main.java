b582720a06f0184d624573eddbc7c7d23a5f061f
Code-JV-export <josevagner9@gmail.com>
2021-12-20 21:38:26 -0300

    adicionando a classe Livro, Artigo, e Publicacao. Falta criar toda a lógica nelas

---
+++ src/com/project/gerenciadorbiblioteca/Main.java
@@ -0,0 +1,8 @@
+package com.project.gerenciadorbiblioteca;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/project/gerenciadorbiblioteca/dominio/Publicacao.java
@@ -0,0 +1,4 @@
+package com.project.gerenciadorbiblioteca.dominio;
+
+public class Publicacao {
+}
